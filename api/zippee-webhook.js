bd830649b106709a67471b2ebb8f4cff23e24327
Santhil Kherwal <72144927+santhil-cyber@users.noreply.github.com>
2026-02-11 21:09:40 +0530

    Update zippee-webhook.js

--- api/zippee-webhook.js
+++ api/zippee-webhook.js
@@ -32,14 +32,13 @@ module.exports = async (req, res) => {
 
        const aisensyData = {
             apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NDdiZDI5OGI0YWI1MGMwN2RiYzk4NiIsIm5hbWUiOiJTbGFwcGluIEZvb2RzIFB2dCBMdGQiLCJhcHBOYW1lIjoiQWlTZW5zeSIsImNsaWVudElkIjoiNjg0N2JkMjk4YjRhYjUwYzA3ZGJjOTgxIiwiYWN0aXZlUGxhbiI6IkJBU0lDX1lFQVJMWSIsImlhdCI6MTc2NjAzOTQ1OH0.OO4Yoj-800AudUM1B8i9IJ78BK_TepVqnmkdPDVuqTM",
-            campaignName: "zippee_aisensy_bridge", // Updated to your specific name
+            campaignName: "zippee_aisensy_bridge", 
             destination: formattedPhone,
             userName: customer_name || "Customer",
             templateParams: [
-                String(customer_name || "Customer"),
-                String(order_number || "N/A"),
-                String(current_status || "Updated"),
-                String(tracking_link || "")
+                String(customer_name || "Customer"), // {{1}}
+                String(order_number || "Order"),    // {{2}}
+                String(order_number || "Order")     // {{3}} - This goes into the Button URL
             ]
         };
 
@@ -54,4 +53,5 @@ module.exports = async (req, res) => {
         });
     }
 };
+
 
