c4effc755f3542be1f1aaff52d0dd8e9ab936ff3
Santhil Kherwal <72144927+santhil-cyber@users.noreply.github.com>
2026-02-12 21:13:58 +0530

    Update zippee-webhook.js

--- api/zippee-webhook.js
+++ api/zippee-webhook.js
@@ -60,7 +60,7 @@ module.exports = async (req, res) => {
 
        
         const aisensyData = {
-            apiKey: process.env.AISENSY_API_KEY,
+            apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NDdiZDI5OGI0YWI1MGMwN2RiYzk4NiIsIm5hbWUiOiJTbGFwcGluIEZvb2RzIFB2dCBMdGQiLCJhcHBOYW1lIjoiQWlTZW5zeSIsImNsaWVudElkIjoiNjg0N2JkMjk4YjRhYjUwYzA3ZGJjOTgxIiwiYWN0aXZlUGxhbiI6IkJBU0lDX1lFQVJMWSIsImlhdCI6MTc2NjAzOTQ1OH0.OO4Yoj-800AudUM1B8i9IJ78BK_TepVqnmkdPDVuqTM",
             campaignName: "zippee_aisensy_bridge",
             destination: formattedPhone,
             userName: customerName || "Customer",
@@ -86,3 +86,4 @@ module.exports = async (req, res) => {
         });
     }
 };
+
