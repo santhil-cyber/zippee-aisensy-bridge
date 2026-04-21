31d297c6ff6694edd04847ec650e8c329026e524
Santhil Kherwal <72144927+santhil-cyber@users.noreply.github.com>
2026-04-21 22:08:26 +0530

    Update zippee-webhook.js

--- api/zippee-webhook.js
+++ api/zippee-webhook.js
@@ -85,7 +85,7 @@ module.exports = async (req, res) => {
         const orderNumber = String(orderNo);
 
         const aisensyData = {
-            apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NDdiZDI5OGI0YWI1MGMwN2RiYzk4NiIsIm5hbWUiOiJTbGFwcGluIEZvb2RzIFB2dCBMdGQiLCJhcHBOYW1lIjoiQWlTZW5zeSIsImNsaWVudElkIjoiNjg0N2JkMjk4YjRhYjUwYzA3ZGJjOTgxIiwiYWN0aXZlUGxhbiI6IkJBU0lDX1lFQVJMWSIsImlhdCI6MTc2NjAzOTQ1OH0.OO4Yoj-800AudUM1B8i9IJ78BK_TepVqnmkdPDVuqTM",
+            apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NDdiZDI5OGI0YWI1MGMwN2RiYzk4NiIsIm5hbWUiOiJTbGFwcGluIEZvb2RzIFB2dCBMdGQiLCJhcHBOYW1lIjoiQWlTZW5zeSIsImNsaWVudElkIjoiNjg0N2JkMjk4YjRhYjUwYzA3ZGJjOTgxIiwiYWN0aXZlUGxhbiI6IkJBU0lDX1lFQVJMWSIsImlhdCI6MTc3Njc4OTQ4NH0.ABs5XchvFj5U1D4PUvMK2lGvJuOj_c340Bl7oMy-WMc",
             campaignName: "delivery_tracking",
             destination: formattedPhone,
             userName: name,
