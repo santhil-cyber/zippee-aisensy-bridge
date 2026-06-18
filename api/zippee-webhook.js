0df71ea80d095ec30efdbf4e38be54cc51c39e63
Santhil Kherwal <72144927+santhil-cyber@users.noreply.github.com>
2026-06-18 21:43:03 +0530

    feat: update campaignName to delivery_tracking_v4

--- api/zippee-webhook.js
+++ api/zippee-webhook.js
@@ -110,7 +110,7 @@ module.exports = async (req, res) => {
 
         const aisensyData = {
             apiKey: apiKey,
-            campaignName: "delivery_tracking_v3",
+            campaignName: "delivery_tracking_v4",
             destination: formattedPhone,
             userName: name,
             templateParams: [
